1de4216986a25aed662e826ec8ae9e81535b6b9f
CareerHubapp <helpcarecareerhub123@gmail.com>
2020-06-14 17:41:37 +0530

    first commit

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import os
+import cv2
+from WordSegmentation import wordSegmentation, prepareImg
+
+
+def main():
+
+	imgFiles = os.listdir('../data/')
+	for (i,f) in enumerate(imgFiles):
+		print('Segmenting words of sample %s'%f)
+
+		img = prepareImg(cv2.imread('../data/%s'%f), 50)
+
+		res = wordSegmentation(img, kernelSize=25, sigma=11, theta=7, minArea=100)
+		
+
+		if not os.path.exists('../out/%s'%f):
+			os.mkdir('../out/%s'%f)
+		
+
+		print('Segmented into %d words'%len(res))
+		for (j, w) in enumerate(res):
+			(wordBox, wordImg) = w
+			(x, y, w, h) = wordBox
+			cv2.imwrite('../out/%s/%d.png'%(f, j), wordImg)
+			cv2.rectangle(img,(x,y),(x+w,y+h),0,1)
+		
+
+		cv2.imwrite('../out/%s/summary.png'%f, img)
+
+
+if __name__ == '__main__':
+	main()
